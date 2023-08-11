ab157fe0dd34c35c76ce138efb00a4b2db458cd8
iJaySharma <jay.sharma96100@gmail.com>
2023-08-11 23:31:44 +0530

    msg

---
+++ src/main/java/com/thinking/machines/msgboard/dao/AdministratorDAO.java
@@ -0,0 +1,24 @@
+package com.thinking.machines.msgboard.dao;
+import java.sql.*;
+public class AdministratorDAO
+{
+public void add(Adminitrator administrator)
+{
+try
+{
+Connection connection=DAOConnection.getConnection();
+PreparedStatement preparedStatement;
+preparedStatement=connection.prepareStatement("inser into administartor values(?,?,?)");
+preparedStatement.setString(1,administrator.getUsername());
+preparedStatement.setString(2,administrator.getPassword());
+preparedStatement.setString(3.administrator.getPasswordKey());
+preparedStatement.executeUpdate();
+preparedStatement.close();
+connection.close();
+}catch(SQLException sqlException)
+{
+System.out.println(sqlException); // latyer on add to log an remove this
+throw new DAOException("Unable to add administration");
+}
+}
+}
